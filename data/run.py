3f62a65ff50b7a0ee3a017d341174fdee6701060
Guilherme Manteigas <u04gd19@abdn.ac.uk>
2020-12-01 17:46:12 +0000

    Update run.py

--- data/run.py
+++ data/run.py
@@ -1,6 +1,6 @@
-import json
-import urllib.request
-import requests
+#import json
+#import urllib.request
+#import requests
 
 f = open("demofile3.txt", "w+")
 f.write("Woops! I have deleted the content!")
