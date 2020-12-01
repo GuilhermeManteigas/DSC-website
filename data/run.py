1692b30551850a9ea06b817903892449871899b0
Guilherme Manteigas <u04gd19@abdn.ac.uk>
2020-12-01 17:33:25 +0000

    Update run.py

--- data/run.py
+++ data/run.py
@@ -1,12 +1,7 @@
 import json
 import urllib.request
+import requests
 
-with urllib.request.urlopen("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUirrYErHzS2TR20MRAK6BWg&key=AIzaSyAzIUs0f4Mm1tqXOxc6EXIUnZLi2Nfgyns") as url:
-    data = json.loads(url.read().decode())
-	
-#solditems = requests.get('https://github.com/timeline.json') # (your url)
-#data = solditems.json()
-with open('videos.json', 'w+') as f:
-    json.dump(data, f)
-	
-	
+f = open("demofile3.txt", "w+")
+f.write("Woops! I have deleted the content!")
+f.close()
